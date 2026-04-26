ee13e555a3f14410d3728b504d50d6e9b0842898
tfr691245-blip <tfr691245@gmail.com>
2026-04-26 22:15:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-console.log("Server is running!");
+// Simple server to keep the node alive
+console.log("Server Started Successfully!");
+
+// This keeps the process running every 1 minute
 setInterval(() => {
-  console.log("Keep alive...");
+    const memoryUsage = process.memoryUsage().rss / 1024 / 1024;
+    console.log(`Server heartbeat. Memory usage: ${memoryUsage.toFixed(2)} MB`);
 }, 60000);
